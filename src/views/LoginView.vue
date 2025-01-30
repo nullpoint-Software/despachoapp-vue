e5562072138dc65e693ebcba7e295a9bb1e8e465
gil-666 <143546119+gil-666@users.noreply.github.com>
2025-01-30 14:01:24 -0600

    login v0.1

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,45 +1,59 @@
-<script setup lang="ts">
+<script setup>
 import { Button } from 'primevue';
-
+import { ref } from 'vue';
+import { useToast } from "primevue/usetoast";
+import { InputText } from 'primevue';
+const initialValues = ref({
+    username: '',
+    email: ''
+});
 
 </script>
 <template>
     <div class="container">
         <div class="loader">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-    <div class="background">
-        <img src="../assets/img/logobig.png" class="logo">
-    </div>
-    <div class="login-container">
-        <div class="login-form">
-            <input type="text" placeholder="sas">
-            <Button>Login</Button>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="background">
+            <img src="../assets/img/logobig.png" class="logo">
+        </div>
+        <div class="login-container">
+            <Form v-slot="$form" class="flex justify-center flex-col gap-4">
+                <div class="flex flex-col gap-1">
+                    <InputText name="username" type="text" placeholder="Username" />
+
+                </div>
+                <div class="flex flex-col gap-1">
+                    <InputText name="email" type="text" placeholder="Email" />
+
+                </div>
+                <Button type="submit" severity="secondary" label="Submit" />
+            </Form>
         </div>
     </div>
-    </div>
-    
-    
-    
+
+
+
 </template>
 <style lang="css" scoped>
-.container{
+.container {
     position: relative;
 }
-.login-container{
+
+.login-container {
     padding: 10px;
     border-radius: 10px;
     background-color: rgba(255, 255, 255, 0.6);
@@ -54,12 +68,14 @@ import { Button } from 'primevue';
     top: 0;
     right: 0;
 }
-.logo{
+
+.logo {
     place-items: center;
     opacity: 48%;
     z-index: 1;
 }
-.loader{
+
+.loader {
     position: absolute;
 }
 </style>
