348d770e97300477d9e6da8c6b68b2db7c8ce723
gil-666 <143546119+gil-666@users.noreply.github.com>
2025-02-08 23:29:41 -0600

    login respo

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -49,30 +49,36 @@ const initialValues = ref({
 
 </template>
 <style lang="css" scoped>
+
 .container {
+    display: block;
     position: relative;
+    margin: 0 auto;
+    justify-items: center;
 }
 
 .login-container {
     padding: 10px;
     border-radius: 10px;
+    min-height: 200px;
     background-color: rgba(255, 255, 255, 0.6);
     opacity: 100%;
     text-align: center;
     max-width: 300px;
     margin: 0 auto;
     z-index: 2;
-    position: absolute;
+    /* position: absolute; */
     width: 100%;
     height: 100%;
-    top: 0;
-    right: 0;
+    /* top: 0; */
+    /* right: 0; */
 }
 
 .logo {
     place-items: center;
     opacity: 48%;
     z-index: 1;
+    max-height: 500px;
 }
 
 .loader {
