a4e18e38ef5584218e889432b1658fa54caad121
gil-666 <143546119+gil-666@users.noreply.github.com>
2025-02-19 16:21:44 -0600

    clientes tabla v0, remove server

--- src/components/adminApp/Clientes.vue
+++ src/components/adminApp/Clientes.vue
@@ -1,9 +1,31 @@
 <template>
-    <div class="shadow-md  p-4 m-5 bg-stone-100 text-slate-900 h-full w-full">
+    <div class="shadow-md p-4 m-5 bg-stone-100 text-slate-900 h-full w-full">
         <div class="font-bold text-2xl pl-2">Clientes</div>
+
+        <DataTable :value="customers" tableStyle="min-width: 50rem">
+            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></Column>
+        </DataTable>
+
     </div>
 </template>
 <script setup>
-import * as cs from '@/service/adminApp/clienteService';
+import { ref } from "vue";
+import * as cs from "@/service/adminApp/clienteService";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row";
+const customers = [
+    {
+        nombre: "Juan Perez"
+        
+    }]
+
+    const columns = [
+    { field: 'code', header: 'Code' },
+    { field: 'name', header: 'Name' },
+    { field: 'category', header: 'Category' },
+    { field: 'quantity', header: 'Quantity' }
+];
 cs.getClientes();
 </script>
