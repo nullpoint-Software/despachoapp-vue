e6c57dacd067fa4723e59f92ba14e410badc3e09
gil <143546119+gil-666@users.noreply.github.com>
2025-04-14 02:04:28 -0600

    fix script

--- generate-build-time.js
+++ generate-build-time.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Emular __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const buildTime = new Date().toLocaleString('es-MX', {
   dateStyle: 'full',
